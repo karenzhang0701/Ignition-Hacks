b6d1c488fe2841f17d9111201582095e5d1c5bd6
karenzhang0701 <109870617+karenzhang0701@users.noreply.github.com>
2024-08-17 20:49:35 -0400

    testing again

--- chrome-extension/server/scraper.py
+++ chrome-extension/server/scraper.py
@@ -1,11 +1,11 @@
 import sys
-import newspaper
+from newspaper import Article
 import nltk
 # nltk.download('punkt')
 
-url = 'https://www.theglobeandmail.com/world/us-politics/article-will-fear-of-donald-trump-be-enough-to-send-kamala-harris-to-the-white/'
+url = sys.argv[1]
 
-article = newspaper.Article(url)
+article = Article(url)
 article.download()
 
 article.parse()
